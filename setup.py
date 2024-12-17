2bfa86856717738428793544691be0b94d585e1e
nikolasfil <nikolas13in@gmail.com>
2024-12-17 15:00:29 +0200

    working on breaking down the requirements for files

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 import os
+from pathlib import Path
 
 with open("app/README.md", "r") as fh:
     long_description = fh.read()
@@ -8,10 +9,23 @@ version = "0.0.1"
 with open("VERSION", "r") as fv:
     version = fv.read()
 
+# Setup requirements
+
+requirements_path = Path("requirements")
 
 requirements = []
-with open("requirements.txt", "r") as fr:
+with open(Path(requirements_path, "requirements.txt"), "r") as fr:
     requirements = fr.read().splitlines()
+
+
+requirements_doc = []
+with open(Path(requirements_path, "requirements-docs-pdoc3.txt"), "r") as fr:
+    requirements_doc = fr.read().splitlines()
+
+requirements_dev = []
+with open(Path(requirements_path, "requirements.txt"), "r") as fr:
+    requirements_dev = fr.read().splitlines()
+
 
 setuptools.setup(
     name="ctfsolver",
@@ -34,7 +48,7 @@ setuptools.setup(
     # package_data={"ctfsolver": ["files/*"]},
     extras_require={
         "dev": ["pytest>=6.2.4", "twine>=3.4.2"],
-        "docs": ["pdoc3>=0.9.2"],
+        "docs": requirements_doc,
     },
     python_requires=">=3.11",
     entry_points={
