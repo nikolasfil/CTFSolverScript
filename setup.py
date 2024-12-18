62fa5233487f925572c4fa79eab0630863c3f166
nikolasfil <nikolas13in@gmail.com>
2024-12-18 15:04:06 +0200

    updated the setup.py script

--- setup.py
+++ setup.py
@@ -9,23 +9,6 @@ version = "0.0.1"
 with open("VERSION", "r") as fv:
     version = fv.read()
 
-# Setup requirements
-
-requirements_path = Path("requirements")
-
-requirements = []
-with open(Path(requirements_path, "requirements.txt"), "r") as fr:
-    requirements = fr.read().splitlines()
-
-
-requirements_doc = []
-with open(Path(requirements_path, "requirements-docs-pdoc3.txt"), "r") as fr:
-    requirements_doc = fr.read().splitlines()
-
-requirements_dev = []
-with open(Path(requirements_path, "requirements.txt"), "r") as fr:
-    requirements_dev = fr.read().splitlines()
-
 
 setuptools.setup(
     name="ctfsolver",
@@ -43,12 +26,18 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent",
     ],
-    install_requires=requirements,
+    install_requires=[
+        "build>=0.7.0",
+        "pwntools>=4.0.0",
+        "scapy>=2.0.0",
+        "setuptools>=67.7.0",
+        "wheel>=0.37.0",
+    ],
     # namespace_packages=["ctfsolver"],
     # package_data={"ctfsolver": ["files/*"]},
     extras_require={
-        "dev": ["pytest>=6.2.4", "twine>=3.4.2"],
-        "docs": requirements_doc,
+        "dev": ["pytest>=6.2.4", "twine>=3.4.2", "pipdeptree>=2.0.0"],
+        "docs": ["pdoc3>=0.11.4"],
     },
     python_requires=">=3.11",
     entry_points={
